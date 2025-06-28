b01d4b8a5db8fcc9bda775c0a8557bc514d59039
Bruce Wayne <pachade2@gmail.com>
2025-06-28 14:06:20 +0530

    Initial commit of WebDriverMaven project

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void doLogin() {
+        driver.get("https://www.saucedemo.com/");
+
+        //Explicit Wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
+        WebElement password = driver.findElement(By.id("password"));
+
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        driver.quit();
+    }
+}
